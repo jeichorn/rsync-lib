804e835cc76e08ebd7c217fc66e6bf8e05c9aa1b
John Gaffney <john.gaffney@pagely.com>
2024-07-31 10:47:56 -0700

    update tests because of phpunit update

--- tests/AFM/Rsync/Tests/SSHTest.php
+++ tests/AFM/Rsync/Tests/SSHTest.php
@@ -13,131 +13,122 @@ namespace AFM\Rsync\Tests;
 
 use AFM\Rsync\SSH;
 
-class SSHTest extends \PHPUnit_Framework_TestCase
+class SSHTest extends \PHPUnit\Framework\TestCase
 {
-	public function testValidConfiguration()
-	{
-		$fakePublicKey = __DIR__ . '/fake_key.pub';
+    public function testValidConfiguration()
+    {
+        $fakePublicKey = __DIR__ . '/fake_key.pub';
 
-		touch($fakePublicKey);
+        touch($fakePublicKey);
 
-		new SSH(array('port' => 1443, 'public_key' => $fakePublicKey));
+        new SSH(['port' => 1443, 'public_key' => $fakePublicKey]);
 
-		$this->assertTrue(true);
+        $this->assertTrue(true);
 
-		unlink($fakePublicKey);
-	}
+        unlink($fakePublicKey);
+    }
 
-	/**
- 	 * @expectedException \InvalidArgumentException
-	 */
-	public function testInvalidPublicKey()
-	{
-		new SSH(array('public_key' => '/cant/read!'));
-	}
+    public function testInvalidPublicKey()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new SSH(['public_key' => '/cant/read!']);
+    }
 
-	/**
-	 * @expectedException \InvalidArgumentException
-	 */
-	public function testInvalidPortNumber()
-	{
-		new SSH(array('port' => 'not_a_number'));
-	}
+    public function testInvalidPortNumber()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new SSH(['port' => 'not_a_number']);
+    }
 
-	public function testGetConnectionString()
-	{
-		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com'));
+    public function testGetConnectionString()
+    {
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com']);
 
-		$actual = $ssh->getCommand();
-		$expected = "ssh test@test.com";
+        $actual = $ssh->getCommand();
+        $expected = 'ssh test@test.com';
 
-		$this->assertEquals($expected, $actual);
-	}
+        $this->assertEquals($expected, $actual);
+    }
 
-	public function testGetConnectionNonStandardPort()
-	{
-		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com', 'port' => 231));
+    public function testGetConnectionNonStandardPort()
+    {
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com', 'port' => 231]);
 
-		$actual = (string)$ssh->getCommand();
-		$expected = "ssh -p 231 test@test.com";
+        $actual = (string)$ssh->getCommand();
+        $expected = 'ssh -p 231 test@test.com';
 
-		$this->assertEquals($expected, $actual);
-	}
+        $this->assertEquals($expected, $actual);
+    }
 
-	public function testGetConnectionWithPublicKey()
-	{
-		$publicKey = "./key.pub";
-		$publicKeyWithSpaces = "./key key.pub";
+    public function testGetConnectionWithPublicKey()
+    {
+        $publicKey = './key.pub';
+        $publicKeyWithSpaces = './key key.pub';
 
-		touch($publicKey);
-		touch($publicKeyWithSpaces);
+        touch($publicKey);
+        touch($publicKeyWithSpaces);
 
-		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com', 'public_key' => $publicKey));
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com', 'public_key' => $publicKey]);
 
-		$actual = (string)$ssh->getCommand();
-		$expected = "ssh -i '" .$publicKey. "' test@test.com";
+        $actual = (string)$ssh->getCommand();
+        $expected = "ssh -i '" .$publicKey. "' test@test.com";
 
-		$this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
 
-		$ssh->setPublicKey($publicKeyWithSpaces);
+        $ssh->setPublicKey($publicKeyWithSpaces);
 
-		$actual = (string)$ssh->getCommand();
-		$expected = "ssh -i '" .$publicKeyWithSpaces. "' test@test.com";
+        $actual = (string)$ssh->getCommand();
+        $expected = "ssh -i '" .$publicKeyWithSpaces. "' test@test.com";
 
-		$this->assertEquals($expected, $actual);
+        $this->assertEquals($expected, $actual);
 
-		unlink($publicKey);
-		unlink($publicKeyWithSpaces);
-	}
+        unlink($publicKey);
+        unlink($publicKeyWithSpaces);
+    }
 
-	public function testGetHostConnection()
-	{
-		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com'));
+    public function testGetHostConnection()
+    {
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com']);
 
-		$actual = $ssh->getHostConnection();
-		$expected = "test@test.com";
+        $actual = $ssh->getHostConnection();
+        $expected = 'test@test.com';
 
-		$this->assertEquals($expected, $actual);
-	}
+        $this->assertEquals($expected, $actual);
+    }
 
-	public function testGetConnectionOptions()
-	{
-		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com', 'port' => 231, 'public_key' => '/dev/null'));
+    public function testGetConnectionOptions()
+    {
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com', 'port' => 231, 'public_key' => '/dev/null']);
 
-		$actual = $ssh->getConnectionOptions();
-		$expected = "ssh -p 231 -i '/dev/null'";
+        $actual = $ssh->getConnectionOptions();
+        $expected = "ssh -p 231 -i '/dev/null'";
 
-		$this->assertEquals($expected, $actual);
-	}
+        $this->assertEquals($expected, $actual);
+    }
 
-	/**
-	 * @expectedException \InvalidArgumentException
-	 */
-	public function testGetConnectionNoUsername()
-	{
-		$ssh = new SSH;
+    public function testGetConnectionNoUsername()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $ssh = new SSH();
 
-		$ssh->getCommand();
-	}
+        $ssh->getCommand();
+    }
 
-	/**
-	 * @expectedException \InvalidArgumentException
-	 */
-	public function testGetConnectionNoHost()
-	{
-		$ssh = new SSH(array('username' => 'test'));
+    public function testGetConnectionNoHost()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $ssh = new SSH(['username' => 'test']);
 
-		$ssh->getCommand();
-	}
+        $ssh->getCommand();
+    }
 
-	public function testSetExecutable()
-	{
-  		$ssh = new SSH(array('username' => 'test', 'host' => 'test.com', 'port' => 231, 'executable' => 'c:/cygwin/bin/ssh.exe'));
+    public function testSetExecutable()
+    {
+        $ssh = new SSH(['username' => 'test', 'host' => 'test.com', 'port' => 231, 'executable' => 'c:/cygwin/bin/ssh.exe']);
 
-	        $actual   = $ssh->getConnectionOptions();
-	        $expected = "c:/cygwin/bin/ssh.exe -p 231";
+        $actual   = $ssh->getConnectionOptions();
+        $expected = 'c:/cygwin/bin/ssh.exe -p 231';
 
-        	$this->assertEquals($expected, $actual);
-	}
+        $this->assertEquals($expected, $actual);
+    }
 }
-
